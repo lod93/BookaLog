1af17b4dfececd9b46b9c80580706ec3b9ef3f39
lod93 <problematicdelegate@gmail.com>
2021-06-12 17:47:14 +0200

    input

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,30 @@
 import React,{Component} from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 class Search extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+  }
+  state = {
+    query: ''
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query.trim()
+    }))
+  }
+  clearQuery = () => {
+    this.updateQuery('')
+  }
+
   render(){
+    const { query } = this.state
+    const { books } = this.props
+    const showingBooks = query === ''
+    ? books
+    : books.filter((c) => (
+        c.name.toLowerCase().includes(query.toLowerCase())
+      ))
     return (
       <div className="search-books">
       <div className="search-books-bar">
